e1ae908d44b78efa090cf24d4c834bd7c6aa118e
Amy Nguyen <amy.nguyen730@gmail.com>
2019-08-29 00:23:55 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {Beyonce = "Halo", Adele = "Hello", Keys = "No one"}
+
+function updatePlaylist (obj, key, value) {
+  return object.assign ({} , playlist, {[Anime], 'One punch theme'})
+}
+
+function removeFromPlaylist (obj, key) {
+  delete playlist.Adele
+}
