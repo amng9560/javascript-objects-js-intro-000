ef06e94168ca38775ace842cc341ee02970cc337
Amy Nguyen <amy.nguyen730@gmail.com>
2019-08-29 00:26:16 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {beyonce = "halo", adele = "hello", keys = "no one"}
+var playlist = new object {beyonce = "halo", adele = "hello", keys = "no one"}
 
 function updatePlaylist (obj, key, value) {
   return object.assign ({} , playlist, {[Anime], 'One punch theme'})
